ea306d01ec0de8f9eb4921a04024d5b243f364e3
Sam <Samsawesome2@gmail.com>
2017-07-31 15:21:44 +0530

    Stats
    Because apparently this is going to be a traditional RPG.

--- Game.java
+++ Game.java
@@ -41,3 +41,12 @@ public class Game implements ActionListener {
 	}
 	
 }
+
+public class Stats{ 
+	double strength = 10;
+	double intelligence = 15;
+	double charisma = 5;
+	double morale = 25;
+	double dexterity = 10;
+	
+}
