b3b6082e563a57969b56b5980e1f5cc329a71825
Sam <Samsawesome2@gmail.com>
2017-07-31 15:02:26 +0530

    We finshed the game.
    I did it.

--- Game.java
+++ Game.java
@@ -2,7 +2,10 @@
 public class Game {
 
 	public static void main(String[] args) {
-		
+		int	hduhudhudhaudhauhaudhaudahudahudhaudhaudhaduahduahaudhaudhaudhaudhauhaduhaudhaudhuahduadhaudhaudhaudhaudh = 0;
+		while(hduhudhudhaudhauhaudhaudahudahudhaudhaudhaduahduahaudhaudhaudhaudhauhaduhaudhaudhuahduadhaudhaudhaudhaudh <= 15872){
+			System.out.println(hduhudhudhaudhauhaudhaudahudahudhaudhaudhaduahduahaudhaudhaudhaudhauhaduhaudhaudhuahduadhaudhaudhaudhaudh);
+			hduhudhudhaudhauhaudhaudahudahudhaudhaudhaduahduahaudhaudhaudhaudhauhaduhaudhaudhuahduadhaudhaudhaudhaudh++;
 		
 	}
 
