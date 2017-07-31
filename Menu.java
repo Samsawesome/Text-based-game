daa6fceb0bfb06b97c12d3be6403af85c69b8571
recko10 <30591169+recko10@users.noreply.github.com>
2017-07-31 15:18:35 +0530

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,13 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class Menu extends JPanel {
+
+	public void paintComponent(Graphics g)
+	{
+		//If any images are needed
+	}
+	
+}
