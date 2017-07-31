341948fe0368a5892bf2cdb78005240bf169f0ea
recko10 <30591169+recko10@users.noreply.github.com>
2017-07-31 15:17:32 +0530

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,12 +1,43 @@
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Game {
+public class Game implements ActionListener {
 
 	public static void main(String[] args) {
-		int	hduhudhudhaudhauhaudhaudahudahudhaudhaudhaduahduahaudhaudhaudhaudhauhaduhaudhaudhuahduadhaudhaudhaudhaudh = 0;
-		while(hduhudhudhaudhauhaudhaudahudahudhaudhaudhaduahduahaudhaudhaudhaudhauhaduhaudhaudhuahduadhaudhaudhaudhaudh <= 15872){
-			System.out.println(hduhudhudhaudhauhaudhaudahudahudhaudhaudhaduahduahaudhaudhaudhaudhauhaduhaudhaudhuahduadhaudhaudhaudhaudh);
-			hduhudhudhaudhauhaudhaudahudahudhaudhaudhaduahduahaudhaudhaudhaudhauhaduhaudhaudhuahduadhaudhaudhaudhaudh++;
+		Game run = new Game();
+		run.go();
+		
+
+	}
+	
+	//Runs code in this method when a certain button is clicked
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
 		
 	}
 
+	public void go()
+	{
+		//Initializing objects
+		JFrame j = new JFrame();
+		//Menu Button
+		JButton menuButton = new JButton("Start");
+		//Menu JPanel
+				Menu menu = new Menu();
+		//Settings for JFrame
+		j.setVisible(true);
+		j.setSize(1300, 1300);
+		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		j.add(menu);
+		menu.setLayout(null);
+		menu.add(menuButton);
+		menuButton.setBounds(560, 400, 200, 50);
+		
+		
+		
+	}
+	
 }
